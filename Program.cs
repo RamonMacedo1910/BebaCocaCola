8e8f6824ad43c3cd09526d12c41e4476ada93f53
Ramon Mesquita <ramonmesquitamacedo2222@gmail.com>
2023-04-23 00:11:00 -0300

    Exercício concluído

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Poema da Coca Cola!");
+Console.WriteLine();
+        Console.WriteLine("beba coca cola");
+        Console.WriteLine("babe      cola");
+        Console.WriteLine("beba coca");
+        Console.WriteLine("babe cola caco");
+        Console.WriteLine("caco");
+        Console.WriteLine("cola");
+        Console.WriteLine("     cloaca");
